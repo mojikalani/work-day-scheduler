0f06f1485269235791990cceac492a97565bdfcf
kalanimojica <mojica.kalani@gmail.com>
2021-09-30 16:25:19 -0700

    Time block working and is saving events to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,59 @@
-// TODO:
-/*
-    Need to use moment.js to display current time
-    Time: 
-        - Time is displayed at the top of the page. 
-        - When hour passes, row is greyed out
-        - When it is the current time, row is red 
-        - All other future times, row is green
-    
-    Input: 
-    When user types an input: 
-        - Input is saved into the local files after user enters event and clicks save
-        - When user refreshes page, it is saved and still there.
-    
-*/
+
 
 // Display current time
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 $('#currentDay').text(today);
 
 // When hour passes, row is greyed out
-
-function timeInterval(){
-    var time = moment().hour(); 
-    $(".time-block").each(function (i)) { 
-        if (this.)
-    }
-}
-
-
 // when it is the current time, row is red 
 // future times, row is green 
+
+function timeInterval(){
+    var currentTime = moment().hour(); 
+    // Turn
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(blockTime)
+        if (blockTime < currentTime) {
+
+            $(this).addClass("past");
+        }
+        else if (blockTime === currentTime) {
+
+            $(this).addClass("present");
+        }
+        else {
+         
+            $(this).addClass("future");
+
+        }
+
+    })
+  
+    }
+    
+// When user wants to save their input    
+var saveButton= $(".saveBtn"); 
+
+var userEvents = [];
+saveButton.on("click", function() { 
+    
+    
+    var event = $(this).siblings('.description').val(); 
+    var time = $(this).siblings('.hour').text();
+    userEvents.push(time, event);
+    localStorage.setItem("userEvents", JSON.stringify(userEvents));
+    console.log(time)
+    console.log(event);
+})
+
+// Displaying any saved events 
+var storedEvents = JSON.parse(localStorage.getItem("userEvents")); 
+    if (storedEvents !== null) { 
+        userEvents = storedEvents;
+        console.log(storedEvents)
+    }
+
+
 
 timeInterval();
